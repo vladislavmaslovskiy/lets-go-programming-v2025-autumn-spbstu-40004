900b9beaf6b53366abdb8d44870bbbe595b4779f
vladislavmaslovskiy <vladuisik826@gmail.com>
2025-10-09 21:35:52 +0300

    [TASK-2-1] Add adjustTemperature

--- vladislav.maslovskiy/task-2-1/cmd/service/main.go
+++ vladislav.maslovskiy/task-2-1/cmd/service/main.go
@@ -1,2 +1,37 @@
 package main
-func main(){}
+
+import (
+	"errors"
+	"fmt"
+)
+var errOperator = errors.New("invalid operation")
+
+func adjustTempurature(lowTempurature int, highTempurature int, askingTempurature int, operation string) (int, int, error) {
+	if lowTempurature == -1 && highTempurature == -1 {
+		return lowTempurature, highTempurature, nil
+	}
+
+	switch operation {
+	case ">=":
+		if askingTempurature > highTempurature {
+			lowTempurature = -1
+			highTempurature = -1
+		} else if lowTempurature <= askingTempurature && askingTempurature <= highTempurature {
+			lowTempurature = askingTempurature
+		}
+	case "<=":
+		if askingTempurature < lowTempurature {
+			lowTempurature = -1
+			highTempurature = -1
+		} else if lowTempurature <= askingTempurature && askingTempurature <= highTempurature {
+			highTempurature = askingTempurature
+		}
+	default:
+		return lowTempurature, highTempurature, errOperator
+	}
+
+	return lowTempurature, highTempurature, nil
+}
+
+func main()
+{}
