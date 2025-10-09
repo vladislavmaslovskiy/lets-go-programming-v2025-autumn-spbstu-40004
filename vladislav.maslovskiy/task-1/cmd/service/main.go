68794446f7c0ce08bc078f4def174248ee68db39
vladislavmaslovskiy <vladuisik826@gmail.com>
2025-10-09 20:33:48 +0300

    [TASK-1] Added user interaction

--- vladislav.maslovskiy/task-1/cmd/service/main.go
+++ vladislav.maslovskiy/task-1/cmd/service/main.go
@@ -1,2 +1,21 @@
 package main
-func main(){}
+func main(){
+  var firstNum, secondNum, result int
+	var operator string
+	_, err := fmt.Scanln(&firstNum)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&secondNum)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&operator)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+}
