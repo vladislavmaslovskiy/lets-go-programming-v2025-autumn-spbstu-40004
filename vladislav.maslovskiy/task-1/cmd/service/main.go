5530d75613da9f170fc5265a8902447e341b2026
vladislavmaslovskiy <vladuisik826@gmail.com>
2025-10-09 20:45:36 +0300

    [TASK-1] Fix missing

--- vladislav.maslovskiy/task-1/cmd/service/main.go
+++ vladislav.maslovskiy/task-1/cmd/service/main.go
@@ -1,22 +1,30 @@
 package main
-func main(){
-  var firstNum, secondNum, result int
+
+import "fmt"
+
+func main() {
+	var firstNum, secondNum, result int
 	var operator string
+
 	_, err := fmt.Scanln(&firstNum)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
+
 	_, err = fmt.Scanln(&secondNum)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
+
 	_, err = fmt.Scanln(&operator)
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
-  switch operator {
+	}
+
+	switch operator {
 	case "+":
 		result = firstNum + secondNum
 	case "-":
@@ -33,5 +41,7 @@ func main(){
 		fmt.Println("Invalid operation")
 		return
 	}
+
 	fmt.Println(result)
 }
+
