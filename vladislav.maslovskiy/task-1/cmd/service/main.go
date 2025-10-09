a3ada4c818ee5ac3f045adce6bb4202a005daf58
vladislavmaslovskiy <vladuisik826@gmail.com>
2025-10-09 20:37:10 +0300

    [TASK-1] Added math logic

--- vladislav.maslovskiy/task-1/cmd/service/main.go
+++ vladislav.maslovskiy/task-1/cmd/service/main.go
@@ -7,15 +7,31 @@ func main(){
 		fmt.Println("Invalid first operand")
 		return
 	}
-
 	_, err = fmt.Scanln(&secondNum)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-
 	_, err = fmt.Scanln(&operator)
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
+  switch operator {
+	case "+":
+		result = firstNum + secondNum
+	case "-":
+		result = firstNum - secondNum
+	case "/":
+		if secondNum == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		result = firstNum / secondNum
+	case "*":
+		result = firstNum * secondNum
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+	fmt.Println(result)
 }
