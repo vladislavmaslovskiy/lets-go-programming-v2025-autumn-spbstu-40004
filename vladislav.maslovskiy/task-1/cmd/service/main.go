5b5cb7e3f1c0f6d4c060c53160a31f1daa759339
vladislavmaslovskiy <vladuisik826@gmail.com>
2025-10-09 20:53:20 +0300

    [TASK-1] Fix lint

--- vladislav.maslovskiy/task-1/cmd/service/main.go
+++ vladislav.maslovskiy/task-1/cmd/service/main.go
@@ -5,25 +5,21 @@ import "fmt"
 func main() {
 	var firstNum, secondNum, result int
 	var operator string
-
 	_, err := fmt.Scanln(&firstNum)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
-
 	_, err = fmt.Scanln(&secondNum)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-
 	_, err = fmt.Scanln(&operator)
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
-
 	switch operator {
 	case "+":
 		result = firstNum + secondNum
@@ -41,7 +37,6 @@ func main() {
 		fmt.Println("Invalid operation")
 		return
 	}
-
 	fmt.Println(result)
 }
 
