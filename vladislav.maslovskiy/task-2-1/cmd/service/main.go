bd74dd5ec66369f75765e5bd4a04d432ece335d3
vladislavmaslovskiy <vladuisik826@gmail.com>
2025-10-09 22:14:16 +0300

    [TASK-2-1] Fix linter

--- vladislav.maslovskiy/task-2-1/cmd/service/main.go
+++ vladislav.maslovskiy/task-2-1/cmd/service/main.go
@@ -6,71 +6,74 @@ import (
 )
 var errOperator = errors.New("invalid operation")
 
-func adjustTempurature(lowTempurature int, highTempurature int, askingTempurature int, operation string) (int, int, error) {
-	if lowTempurature == -1 && highTempurature == -1 {
-		return lowTempurature, highTempurature, nil
+func adjustTempurature(lowesttemp int, highesttemp int, askingTemp int, operation string) (int, int, error) {
+	if lowesttemp == -1 && highesttemp == -1 {
+		return lowesttemp, highesttemp, nil
 	}
 
 	switch operation {
 	case ">=":
-		if askingTempurature > highTempurature {
-			lowTempurature = -1
-			highTempurature = -1
-		} else if lowTempurature <= askingTempurature && askingTempurature <= highTempurature {
-			lowTempurature = askingTempurature
+		if askingTemp > highesttemp {
+			lowesttemp = -1
+			highesttemp = -1
+		} else if lowesttemp <= askingTemp && askingTemp <= highesttemp {
+			lowesttemp = askingTemp
 		}
 	case "<=":
-		if askingTempurature < lowTempurature {
-			lowTempurature = -1
-			highTempurature = -1
-		} else if lowTempurature <= askingTempurature && askingTempurature <= highTempurature {
-			highTempurature = askingTempurature
+		if askingTemp < lowesttemp {
+			lowesttemp = -1
+			highesttemp = -1
+		} else if lowesttemp <= askingTemp && askingTemp <= highesttemp {
+			highesttemp = askingTemp
 		}
 	default:
-		return lowTempurature, highTempurature, errOperator
+		return lowesttemp, highesttemp, errOperator
 	}
 
-	return lowTempurature, highTempurature, nil
+	return lowesttemp, highesttemp, nil
 }
+
 func processDepart(employeeAmount int, minTempurature int, maxTempurature int) {
 	var (
 		operation  string
-		askingTempurature int
+		askingTemp int
 	)
 
-	lowTempurature := minTempurature
-	highTempurature := maxTempurature
+	lowesttemp := minTempurature
+	highesttemp := maxTempurature
+
 	for range employeeAmount {
-		_, err := fmt.Scanln(&operation, &askingTempurature)
-		if err != nil || askingTempurature < 15 || askingTempurature > 30 {
+		_, err := fmt.Scanln(&operation, &askingTemp)
+		if err != nil || askingTemp < 15 || askingTemp > 30 {
 			fmt.Println("Wrong employee input")
 
 			return
 		}
-		lowTempurature, highTempurature, err = adjustTempurature(lowTempurature, highTempurature, askingTempurature, operation)
+
+		lowesttemp, highesttemp, err = adjustTempurature(lowesttemp, highesttemp, askingTemp, operation)
 		if err != nil {
 			fmt.Println(err)
 
 			return
 		}
-		fmt.Println(lowTempurature)
+
+		fmt.Println(lowesttemp)
 	}
 }
 
 func main() {
 	const (
 		minTempurature = 15
-		highTempurature = 30
+		highesttemp = 30
 	)
-
 	var departmentAmount, employeeAmount int
-
 	_, err := fmt.Scanln(&departmentAmount)
 	if err != nil || departmentAmount < 1 || departmentAmount > 1000 {
 		fmt.Println("Wrong  department amount")
 
 		return
 	}
+
 	for range departmentAmount {
 		_, err = fmt.Scanln(&employeeAmount)
 		if err != nil || employeeAmount < 1 || employeeAmount > 1000 {
@@ -78,6 +81,6 @@ func main() {
 
 			return
 		}
-		processDepart(employeeAmount, minTempurature,highTempurature)
+		processDepart(employeeAmount, minTempurature,highesttemp)
 	}
 }
