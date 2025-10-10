2e44b07b4bc5d554d305816f23fb024091c33de8
qyantor <ilyxaplay220@gmail.com>
2025-10-10 17:11:14 +0300

    [TASK-1]  (#59)
    * [TASK-1] made calculator
    
    * [TASK-1] made 1 var out of 2

---
+++ ilya.ageev/task-1/cmd/service/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		a, b int
+		op   string
+	)
+
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch op {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(a / b)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
---
+++ ilya.ageev/task-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/qyantor/task-1
+
+go 1.22.7
