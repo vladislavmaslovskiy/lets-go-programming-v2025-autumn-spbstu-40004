5e8272388a32e8323d8f196c94bcecfc214ee077
Никита <cherkasov5509@gmail.com>
2025-10-17 16:39:46 +0300

    [TASK-1] (#42)
    * [TASK-1] Initial commit
    
    * [TASK-1] Input data processing
    
    * [TASK-1] Calculating operations
    
    * [TASK-1] Correction of the error text
    
    * [TASK-1] Changed the version

---
+++ nikita.cherkasov/task-1/cmd/service/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var operand1, operand2 int
+	_, err := fmt.Scan(&operand1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&operand2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	var operator string
+	_, err = fmt.Scan(&operator)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+	switch operator {
+	case "+":
+		fmt.Println(operand1 + operand2)
+	case "-":
+		fmt.Println(operand1 - operand2)
+	case "*":
+		fmt.Println(operand1 * operand2)
+	case "/":
+		if operand2 == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(operand1 / operand2)
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
---
+++ nikita.cherkasov/task-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/cherkasoov/task-1
+
+go 1.22.7
