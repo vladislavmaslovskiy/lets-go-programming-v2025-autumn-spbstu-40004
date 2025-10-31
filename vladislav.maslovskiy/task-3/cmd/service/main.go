904d4fcbcbc1ce2df5c00cc2ffb7d9d698b93809
vladislavmaslovskiy <vladuisik826@gmail.com>
2025-10-31 23:17:23 +0300

    [TASK-3]

---
+++ vladislav.maslovskiy/task-3/cmd/service/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"sort"
+
+	configHandler "github.com/vladislavmaslovskiy/task-3/internal/config_handling"
+	ioHandler "github.com/vladislavmaslovskiy/task-3/internal/io_handling"
+	xmlHandler "github.com/vladislavmaslovskiy/task-3/internal/xml_handling"
+)
+
+const (
+	configFlagName         = "config"
+	configFlagDesc         = "Configuration file path"
+	configFlagDefaultValue = ""
+	configFlagPanicMessage = "config file path via flag not specified"
+)
+
+func main() {
+	var configPath string
+
+	flag.StringVar(&configPath, configFlagName, configFlagDefaultValue, configFlagDesc)
+	flag.Parse()
+
+	flagProvided := false
+
+	flag.Visit(func(currentFlag *flag.Flag) {
+		if currentFlag.Name == configFlagName {
+			flagProvided = true
+		}
+	})
+
+	if !flagProvided {
+		panic(configFlagPanicMessage)
+	}
+
+	settings, err := configHandler.LoadConfig(configPath)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	currencyData, err := xmlHandler.ParseXML(settings.InputFile)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	sort.Slice(currencyData, func(firstIndex, secondIndex int) bool {
+		return currencyData[firstIndex].Value > currencyData[secondIndex].Value
+	})
+
+	jsonData, err := json.MarshalIndent(currencyData, "", "\t")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = ioHandler.WriteStringToFile(settings.OutputFile, jsonData)
+	if err != nil {
+		panic(err.Error())
+	}
+}
