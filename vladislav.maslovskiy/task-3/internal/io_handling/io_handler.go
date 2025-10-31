b5de08db247f010a2fffcd3fb352c3adb7710254
vladislavmaslovskiy <vladuisik826@gmail.com>
2025-10-31 23:20:25 +0300

    [TASK-3]Fix

---
+++ vladislav.maslovskiy/task-3/internal/io_handling/io_handler.go
@@ -0,0 +1,50 @@
+package iohandler
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func CreateDirectories(filePath string) error {
+	pathComponents := strings.Split(filePath, "/")
+	if len(pathComponents) == 1 {
+		return nil
+	}
+
+	directoryPath := strings.Join(pathComponents[:len(pathComponents)-1], "/")
+
+	err := os.MkdirAll(directoryPath, os.ModePerm)
+
+	if err != nil {
+		return fmt.Errorf("i/o folders: %w", err)
+	} else {
+		return nil
+	}
+}
+
+func WriteDataToFile(filePath string, content []byte) error {
+	err := CreateDirectories(filePath)
+	if err != nil {
+		return err
+	}
+
+	fileHandle, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("i/o: %w", err)
+	}
+
+	defer func() {
+		if err := fileHandle.Close(); err != nil {
+			panic(err.Error())
+		}
+	}()
+
+	_, err = fileHandle.Write(content)
+	if err != nil {
+		return fmt.Errorf("i/o: %w", err)
+	} else {
+		return nil
+	}
+}
+
