e50d3a051f0f956565358a0d41f57397fdd651bc
vladislavmaslovskiy <vladuisik826@gmail.com>
2025-10-31 21:28:05 +0300

    [TASK-1] Fix

--- vladislav.maslovskiy/task-1/cmd/service/main.go
+++ vladislav.maslovskiy/task-1/cmd/service/main.go
@@ -1,82 +1,46 @@
 package main
 
-
 import "fmt"
 
 func main() {
-	var (
-		firstNumber, secondNumber, res int
-		operand                        string
-	)
-	_, err := fmt.Scanln(&firstNumber)
-func main(){
-  var firstNum, secondNum, result int
-import "fmt"
+	var firstNum, secondNum, result int
+	var operator                    string
 
-func main() {
-	var firstNumber, secondNumber, res int
-	var operand string
-	_, err := fmt.Scanln(&firstNumber)
+	_, err := fmt.Scanln(&firstNum)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
-	_, err = fmt.Scanln(&secondNumber)
+
 	_, err = fmt.Scanln(&secondNum)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-	_, err = fmt.Scanln(&operand)
-	if err != nil {
-		fmt.Println("Invalid operation")
-		return
-	}
-	switch operand {
-	case "+":
-		res = firstNumber + secondNumber
-	case "-":
-		res = firstNumber - secondNumber
-	case "/":
-		if secondNumber == 0 {
-			fmt.Println("Division by zero")
-			return
-		}
-		res = firstNumber / secondNumber
-	case "*":
-		res = firstNumber * secondNumber
-	default:
-		fmt.Println("Invalid operation")
-		return
-	}
-
-	fmt.Println(res)
-}
-func main(){}
-
 
 	_, err = fmt.Scanln(&operator)
 	if err != nil {
 		fmt.Println("Invalid operation")
 		return
 	}
-	switch operand {
+
+	switch operator {
 	case "+":
-		res = firstNumber + secondNumber
+		result = firstNum + secondNum
 	case "-":
-		res = firstNumber - secondNumber
+		result = firstNum - secondNum
 	case "/":
-		if secondNumber == 0 {
+		if secondNum == 0 {
 			fmt.Println("Division by zero")
 			return
 		}
-		res = firstNumber / secondNumber
+		result = firstNum / secondNum
 	case "*":
-		res = firstNumber * secondNumber
+		result = firstNum * secondNum
 	default:
 		fmt.Println("Invalid operation")
 		return
 	}
-	fmt.Println(res)
-}
 
+	fmt.Println(result)
+}
